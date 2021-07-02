2b79d568b0a7905be9e07bd7757d9ae5f3ebad6a
SJAljawhary <sndjehad@gmail.com>
2021-07-02 22:07:33 +0300

    editing my portfolio

--- src/views/examples/Landing.js
+++ src/views/examples/Landing.js
@@ -170,7 +170,7 @@ class Landing extends React.Component {
                       <Card className="card-lift--hover shadow border-0">
                         <CardBody className="py-5">
                         <div className="icon icon-shape icon-shape-primary rounded-circle mb-4">
-                            <i className="fa fa-cookie" />
+                            <i className="fa fa-search" />
                           </div>
                           <h6 className="text-success text-uppercase">
                             Salmon Cookies
